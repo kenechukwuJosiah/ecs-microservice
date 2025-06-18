cab6657894aa6137add411cbc0aa2f981136f57c
kenechukwuJosiah <onwe.josiah@appmartgroup.com>
2025-06-18 18:29:36 +0100

    feat: enhance get_message endpoint in app3 to return a random string for improved functionality

--- app3/app.py
+++ app3/app.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+import random
+import string
 
 from . import create_app
 
@@ -14,7 +16,8 @@ def hello() -> str:
 @flask_app.route('/return_message')
 def get_message() -> str:
     ''' endpoint to test AWS ECS Service Connect functionality '''
-    return 'here is a message from flask_app_3!'
+    random_str = ''.join(random.choices(string.ascii_letters + string.digits, k=12))
+    return f'Random string from flask_app_3: {random_str}'
 
 # if __name__ == '__main__':
 #     flask_app.run(host='0.0.0.0')
