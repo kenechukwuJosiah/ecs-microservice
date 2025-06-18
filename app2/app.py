32d130b275af2ddc97521995e657bc1801ee404a
kenechukwuJosiah <onwe.josiah@appmartgroup.com>
2025-06-18 18:21:34 +0100

    feat: update greetings in app2 to use consistent naming for clarity

--- app2/app.py
+++ app2/app.py
@@ -8,13 +8,13 @@ flask_app = create_app()
 @flask_app.route('/')
 def hello() -> str:
     ''' Return a friendly HTTP greeting '''
-    return f'Hello, World from flask app 2! Time is {datetime.now()}'
+    return f'Hello, World from flask app two! Time is {datetime.now()}'
 
 
 @flask_app.route('/return_message')
 def get_message() -> str:
     ''' endpoint to test AWS ECS Service Connect functionality '''
-    return 'here is a message from flask_app_2!'
+    return 'here is a message from flask_app_two!'
 
 # if __name__ == '__main__':
 #     flask_app.run(host='0.0.0.0')
